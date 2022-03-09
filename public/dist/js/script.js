ea9c2b637230bc8b2cbaa5b95e8125060bf3d8ee
Aziz <Jakiro66@mail.ru>
2022-03-09 13:44:24 +0500

    asd

---
+++ public/dist/js/script.js
@@ -0,0 +1,8 @@
+window.addEventListener('load', function (e) {
+    if ($("#cardTo_table").DataTable().length > 0) {
+        $("#cardTo_table").DataTable()
+    }
+    if ($("#sliderTop_table").DataTable().length > 0) {
+        $("#sliderTop_table").DataTable()
+    }
+});
